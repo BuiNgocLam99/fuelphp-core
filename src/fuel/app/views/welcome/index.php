2ff94a00deaaf202f42f996da10c3c5e37fc76d7
BuiNgocLam99 <113894278+BuiNgocLam99@users.noreply.github.com>
2025-01-19 22:25:56 +0700

    Update index.php

--- src/fuel/app/views/welcome/index.php
+++ src/fuel/app/views/welcome/index.php
@@ -38,6 +38,7 @@
 	<div class="container">
 		<div class="jumbotron">
 			<h1>Welcome!</h1>
+			// test stash
 			<p>You have successfully installed the Fuel PHP Framework.</p>
 			<p><a class="btn btn-primary btn-lg" href="https://docs.fuelphp.com">Read the Docs</a></p>
 		</div>
